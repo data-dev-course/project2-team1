906c87f041cbbe58417655aa7160a70a50d06501
Dahye K <kangdiamond@gmail.com>
2023-06-02 20:54:48 +0900

    update charts

--- strayanimal/src/components/Intro.js
+++ strayanimal/src/components/Intro.js
@@ -1,6 +1,7 @@
+import { useEffect } from "react";
 import "../css/Intro.css";
 import Overview from "./Overview";
-import { useLoaderData } from "react-router-dom";
+
 
 function IntroTop() {
     return (
@@ -8,25 +9,36 @@ function IntroTop() {
         <hr className='intro-orange-line for-not-mobile' style={{left:-10}}></hr>
         <hr className='intro-orange-line for-not-mobile' style={{right:-10}}></hr>
         <div className="intro-typography">
-          Animal<br></br>Stat-us<br></br>Tracker
+            Animal<br></br>Stat-us<br></br>Tracker
         </div>
         <div className="intro-subtitle">
-          유기동물 및 반려동물 통계 및 정보
+            유기동물 및 반려동물 통계 및 정보
         </div>
         <button className='next-page-button bounce2 ' style={{marginTop:"40px", position:"absolute", bottom: 40}}>
-          <i class="fa-solid fa-arrow-down fa-2xl" style={{color: "#FF5F15"}}></i>
+            <i className="fa-solid fa-arrow-down fa-2xl" style={{color: "#FF5F15"}}></i>
         </button>
     </div>
     );
 }
 
 function Intro() {
+    //const { status, data, error } = useQuery(["chart", "1"], async () => {
+    //    const q = await getDocs(collection(db, "strayanimal"));
+    //    const docList = q.docs.map((doc) => {
+    //        const data = doc.data();
+    //        return {
+    //            id: doc.id,
+    //            ...data,
+    //        };
+    //    })
+    //    return docList
+    //});
     return (
-        <div className="intro-container vertical">
+        <div className="intro-container vertical" style={{width:"100%", maxWidth:"1280px"}}>
             <IntroTop/>
             <Overview/>
         </div>
-    )
+    );
 }
 
 export default Intro;
---
+++ strayanimal/src/components/Loading.js
@@ -0,0 +1,10 @@
+import '../css/Loading.css';
+
+function Loading () {
+    return (
+    <div className="ring">
+        <span></span>
+    </div>
+    );
+}
+export default Loading;
