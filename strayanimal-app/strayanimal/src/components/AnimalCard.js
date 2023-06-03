96c9d76b490a18d990e49808f2e28530523fe49e
Dahye K <kangdiamond@gmail.com>
2023-06-03 23:10:59 +0900

    animal-list-page update, pagenation

--- strayanimal-app/strayanimal/src/components/AnimalCard.js
+++ strayanimal-app/strayanimal/src/components/AnimalCard.js
@@ -1,13 +1,15 @@
 import "../css/AnimalCard.css";
 function AnimalCard(params) {
+    const {age, weight, colorCd, sexCd, careNm, status, kind, src} = params
     return (
         <div className="animal-card">
-            <img className="animal-image" src={params} alt=""/>
+            <img className="animal-image" src={src} alt=""/>
             <div className="animal-card-desc vertical">
-                <div style={{fontWeight:600}}>종류</div><div>[개] 믹스견</div>
-                <div style={{fontWeight:600}}>나이</div><div>2020년생</div>
-                <div style={{fontWeight:600}}>상태</div><div>피부질환</div>
-                <div style={{fontWeight:600}}>보호소 위치</div><div>하나동물병원 (전라북도 남원시 왕정동 67)</div>
+                <div style={{fontWeight:600}}>종류</div><div>{kind}</div>
+                <div style={{fontWeight:600}}>나이/무게</div><div>{age}살 / {weight}</div>
+                <div style={{fontWeight:600}}>성별/색깔</div><div>{sexCd ==="F"?'암컷':"수컷"}/{colorCd}</div>
+                <div style={{fontWeight:600}}>상태</div><div>{status}</div>
+                <div style={{fontWeight:600}}>보호소 위치</div><div>{careNm}</div>
             </div>
         </div>
     );
