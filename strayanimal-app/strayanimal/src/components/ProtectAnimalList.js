615fe97b3daa2926a897659237b03059ebb30b90
Dahye K <kangdiamond@gmail.com>
2023-06-08 01:13:28 +0900

    update animal cards style

--- strayanimal-app/strayanimal/src/components/ProtectAnimalList.js
+++ strayanimal-app/strayanimal/src/components/ProtectAnimalList.js
@@ -2,7 +2,7 @@ import AnimalCard from "./AnimalCard";
 import Loading from './Loading';
 import { useQuery} from "@tanstack/react-query";
 import db from '../firebase.js';
-import { getDoc, collection, doc, query, where, documentId } from "firebase/firestore";
+import { getDoc, doc} from "firebase/firestore";
 import "../css/Overview.css";
 import "../css/AnimalCard.css";
 import { Link, Outlet, useParams, useOutletContext } from "react-router-dom";
@@ -27,15 +27,15 @@ export function InnerProtectAnimalList() {
         return <div style={{height:"200px", width:"100%"}}><Loading/></div>
     }
     return (
-        <div className="protect-animal-list" style={{margin:"0 0 20px 0"}}>
+        <div className="protect-animal-list">
             <div className="horizontal align-start" style={{width:"100%"}}>
                 <button className="type-select-button" onClick={()=>setKindType("개")} style={{backgroundColor: kindType==="개"? "#FF5F15": "#4E4E4E"}}>개</button>
                 <button className="type-select-button" onClick={()=>setKindType("고양이")} style={{backgroundColor: kindType==="고양이"? "#FF5F15": "#4E4E4E"}}>고양이</button>
             </div>
             {data.slice((pageNum-1)*10, pageNum*10).map((elem) => 
                 <AnimalCard key={elem.desertionNo} src={elem.popfile} age={elem.age} color={elem.colorCd}
-                careNm={elem.careNm+"("+elem.careAddr+")"} 
-                kind={elem.kindCd+" "+elem.kindSpcs}
+                careNm={elem.careNm+" ( "+elem.careAddr+" )"} 
+                kind={elem.kindSpcs}
                 sexCd={elem.sexCd} status={elem.specialMark} weight={elem.weight}
                 />
             )}
@@ -60,15 +60,18 @@ function ProtectAnimalList() {
             setHasNext(true);
         }
     },[pageNum])
+
+    useEffect(() => {
+        window.scrollTo(0, 0)
+    }, [pageNum])
     return (
         <div className="protect-animal-list-wrap vertical align-start" style={{width:"100%", maxWidth:"1240px", padding:"20px"}}>
             <div className="highlight-title" style={{transform: "translateY(0)", padding:"0 20px"}}>유기동물 공고 목록</div>
             <div className="number-highlight-line" style={{margin: "20px 0"}}></div>
             <Outlet context={[kindType, setKindType]}/>
             <div className="horizontal align-center justify-center-wrap" style={{width:"100%", margin:"0 0 60px 0"}}>
-            {
-                hasBefore?<Link to={`/protect-animal-list/${parseInt(pageNum)-1}`} className="page-button type-select-button">이전</Link>:""
-            }
+            { hasBefore?<Link to={"/protect-animal-list/1"} className="page-button type-select-button">처음</Link>:""}
+            { hasBefore?<Link to={`/protect-animal-list/${parseInt(pageNum)-1}`} className="page-button type-select-button">이전</Link>:""}
             <div className="page-button type-select-button">{pageNum}</div>
             {
                 hasNext?<Link to={`/protect-animal-list/${parseInt(pageNum)+1}`} className="page-button type-select-button">다음</Link>:""
