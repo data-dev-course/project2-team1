c6161edb7a573625497fefa45229fd55a91922ce
Dahye K <kangdiamond@gmail.com>
2023-06-30 01:35:17 +0900

    js prettier apply

--- strayanimal-app/strayanimal/src/components/KoreaMap.js
+++ strayanimal-app/strayanimal/src/components/KoreaMap.js
@@ -1,173 +1,187 @@
-import React, { useEffect, useRef } from 'react';
-import * as d3 from 'd3';
-import { feature } from 'topojson-client';
-import korea from '../assets/korea-topo.json';
-import Loading from './Loading';
-import { useQuery} from "@tanstack/react-query";
-import db from '../firebase.js';
+import React, { useEffect, useRef } from "react";
+import * as d3 from "d3";
+import { feature } from "topojson-client";
+import korea from "../assets/korea-topo.json";
+import Loading from "./Loading";
+import { useQuery } from "@tanstack/react-query";
+import db from "../firebase.js";
 import { getDoc, doc } from "firebase/firestore";
 
-const featureData = feature(korea, korea.objects['korea-topo']);
+const featureData = feature(korea, korea.objects["korea-topo"]);
 
 const KoreaMap = () => {
-    const chart = useRef(null);
-    const {status, data} = useQuery(["strayanimal", "koreamap"], async () => {
-        const docSnap = await getDoc(doc(db, "strayanimal", "차트08_모든_날짜별_분기별_지역별_유기동물_발생_횟수"));
-        return docSnap.data().data;
-    })
-    const printD3 = (datasets) => {
-        const width = 400;
-        const height = 600;
-        const projection = d3.geoMercator().scale(1).translate([0, 0]);
-        const path = d3.geoPath().projection(projection);
-        const bounds = path.bounds(featureData);
-        const dx = bounds[1][0] - bounds[0][0];
-        const dy = bounds[1][1] - bounds[0][1];
-        const x = (bounds[0][0] + bounds[1][0]) / 2;
-        const y = (bounds[0][1] + bounds[1][1]) / 2;
-        const scale = 0.9 / Math.max(dx / width, dy / height);
-        const translate = [width / 2 - scale * x, height / 2 - scale * y];
-        projection.scale(scale).translate(translate);
+	const chart = useRef(null);
+	const { status, data } = useQuery(["strayanimal", "koreamap"], async () => {
+		const docSnap = await getDoc(
+			doc(
+				db,
+				"strayanimal",
+				"차트08_모든_날짜별_분기별_지역별_유기동물_발생_횟수"
+			)
+		);
+		return docSnap.data().data;
+	});
+	const printD3 = (datasets) => {
+		const width = 400;
+		const height = 600;
+		const projection = d3.geoMercator().scale(1).translate([0, 0]);
+		const path = d3.geoPath().projection(projection);
+		const bounds = path.bounds(featureData);
+		const dx = bounds[1][0] - bounds[0][0];
+		const dy = bounds[1][1] - bounds[0][1];
+		const x = (bounds[0][0] + bounds[1][0]) / 2;
+		const y = (bounds[0][1] + bounds[1][1]) / 2;
+		const scale = 0.9 / Math.max(dx / width, dy / height);
+		const translate = [width / 2 - scale * x, height / 2 - scale * y];
+		projection.scale(scale).translate(translate);
 
-        const colorScale = d3.scaleLinear()
-            .domain([d3.max(datasets, d => parseInt(d.count)), 0])
-            .range(['#FF5F15', '#f0f0f0']);
-        
-        d3.select("svg").remove(); 
-        const svg = d3
-            .select(chart.current)
-            .append('svg')
-            .attr('width', width)
-            .attr('height', height);
+		const colorScale = d3
+			.scaleLinear()
+			.domain([d3.max(datasets, (d) => parseInt(d.count)), 0])
+			.range(["#FF5F15", "#f0f0f0"]);
 
-        const mapLayer = svg.append('g');
+		d3.select("svg").remove();
+		const svg = d3
+			.select(chart.current)
+			.append("svg")
+			.attr("width", width)
+			.attr("height", height);
 
-        mapLayer
-            .selectAll('path')
-            .data(featureData.features)
-            .enter()
-            .append('path')
-            .attr('d', path)
-            .style('fill', d => {
-                const name = d.properties.CTP_KOR_NM;
-                const matchingData = datasets.find(item => item.name === name);
-                
-                if (matchingData) {
-                    const count = parseInt(matchingData.count);
-                    return colorScale(count);
-                } else {
-                    return '#666';
-                }
-            });
+		const mapLayer = svg.append("g");
 
-        mapLayer
-            .selectAll('text')
-            .data(featureData.features)
-            .enter()
-            .append('text')
-            .attr('x', d => path.centroid(d)[0])
-            .attr('y', d => path.centroid(d)[1])
-            .attr('text-anchor', 'middle')
-            .attr('alignment-baseline', 'central')
-            .attr('font-size', '12px')
-            .style('fill', '#fff')
-            .text(d => '');
+		mapLayer
+			.selectAll("path")
+			.data(featureData.features)
+			.enter()
+			.append("path")
+			.attr("d", path)
+			.style("fill", (d) => {
+				const name = d.properties.CTP_KOR_NM;
+				const matchingData = datasets.find(
+					(item) => item.name === name
+				);
 
-        const legend = svg.append('g')
-            .attr('class', 'legend')
-            .attr('transform', 'translate(10, 10)');
+				if (matchingData) {
+					const count = parseInt(matchingData.count);
+					return colorScale(count);
+				} else {
+					return "#666";
+				}
+			});
 
-        const legendItemHeight = 20;
-        const legendItems = legend.selectAll('.legend-item')
-            .data(colorScale.ticks(5))
-            .enter()
-            .append('g')
-            .attr('class', 'legend-item')
-            .attr('transform', (d, i) => `translate(0, ${i * legendItemHeight+10})`)
-            .style("fill", "#Fff");
+		mapLayer
+			.selectAll("text")
+			.data(featureData.features)
+			.enter()
+			.append("text")
+			.attr("x", (d) => path.centroid(d)[0])
+			.attr("y", (d) => path.centroid(d)[1])
+			.attr("text-anchor", "middle")
+			.attr("alignment-baseline", "central")
+			.attr("font-size", "12px")
+			.style("fill", "#fff")
+			.text((d) => "");
 
-        legendItems.append('rect')
-            .attr('width', 10)
-            .attr('height', 10)
-            .style('fill', d => colorScale(d));
+		const legend = svg
+			.append("g")
+			.attr("class", "legend")
+			.attr("transform", "translate(10, 10)");
 
-        legendItems.append('text')
-            .attr('x', 20)
-            .attr('y', 9)
-            .attr('font-size', '12px')
-            .text(d => d);
+		const legendItemHeight = 20;
+		const legendItems = legend
+			.selectAll(".legend-item")
+			.data(colorScale.ticks(5))
+			.enter()
+			.append("g")
+			.attr("class", "legend-item")
+			.attr(
+				"transform",
+				(d, i) => `translate(0, ${i * legendItemHeight + 10})`
+			)
+			.style("fill", "#Fff");
 
-        mapLayer
-            .selectAll('path')
-            .on('mouseover', function (event, d) {
-                const [x, y] = d3.pointer(event);
-                const name = d.properties.CTP_KOR_NM;
-                const matchingData = datasets.find(item => item.name === name);
+		legendItems
+			.append("rect")
+			.attr("width", 10)
+			.attr("height", 10)
+			.style("fill", (d) => colorScale(d));
 
-                let value = 'No data';
-                if (matchingData) {
-                    value = matchingData.count;
-                }
+		legendItems
+			.append("text")
+			.attr("x", 20)
+			.attr("y", 9)
+			.attr("font-size", "12px")
+			.text((d) => d);
 
-                d3.select(this)
-                    .style('fill', '#00B3A0');
+		mapLayer.selectAll("path").on("mouseover", function (event, d) {
+			const [x, y] = d3.pointer(event);
+			const name = d.properties.CTP_KOR_NM;
+			const matchingData = datasets.find((item) => item.name === name);
 
-                const box = svg.append('g')
-                    .attr('class', 'value-box')
-                    .attr('transform', `translate(${x},${y - 30})`);
+			let value = "No data";
+			if (matchingData) {
+				value = matchingData.count;
+			}
 
-                const boxWidth = 8 * 20;
-                const boxHeight = 2 * 13;
-                const boxX = -boxWidth / 2;
-                const boxY = -boxHeight / 2;
+			d3.select(this).style("fill", "#00B3A0");
 
-                box.append('rect')
-                    .attr('class', 'value-box')
-                    .attr('x', boxX)
-                    .attr('y', boxY)
-                    .attr('width', boxWidth)
-                    .attr('height', boxHeight)
-                    .attr('rx', 2)
-                    .attr('ry', 2)
-                    .style('fill', '#Fafafa');
+			const box = svg
+				.append("g")
+				.attr("class", "value-box")
+				.attr("transform", `translate(${x},${y - 30})`);
 
-                box.append('text')
-                    .attr('class', 'value')
-                    .attr('text-anchor', 'middle')
-                    .attr('alignment-baseline', 'middle')
-                    .attr('font-size', '14px')
-                    .attr('dy', 0)
-                    .text(name+": "+value);
+			const boxWidth = 8 * 20;
+			const boxHeight = 2 * 13;
+			const boxX = -boxWidth / 2;
+			const boxY = -boxHeight / 2;
 
-                d3.select(this)
-                    .on('mouseout', function () {
-                        d3.select(this)
-                            .style('fill', d => {
-                                const name = d.properties.CTP_KOR_NM;
-                                const matchingData = datasets.find(item => item.name === name);
+			box.append("rect")
+				.attr("class", "value-box")
+				.attr("x", boxX)
+				.attr("y", boxY)
+				.attr("width", boxWidth)
+				.attr("height", boxHeight)
+				.attr("rx", 2)
+				.attr("ry", 2)
+				.style("fill", "#Fafafa");
 
-                                if (matchingData) {
-                                    const count = parseInt(matchingData.count);
-                                    return colorScale(count);
-                                } else {
-                                    return '#666';
-                                }
-                            });
+			box.append("text")
+				.attr("class", "value")
+				.attr("text-anchor", "middle")
+				.attr("alignment-baseline", "middle")
+				.attr("font-size", "14px")
+				.attr("dy", 0)
+				.text(name + ": " + value);
 
-                        svg.select('.value-box').remove();
-                    });
-            });
-    };
+			d3.select(this).on("mouseout", function () {
+				d3.select(this).style("fill", (d) => {
+					const name = d.properties.CTP_KOR_NM;
+					const matchingData = datasets.find(
+						(item) => item.name === name
+					);
 
-    useEffect(() => {
-        if (status === "success" && data !== undefined) {
-            printD3(data);
-        }
-    }, [status, data]);
-    if (status === "loading") {
-        return <Loading/>;
-    }
-    return <div ref={chart}></div>;
+					if (matchingData) {
+						const count = parseInt(matchingData.count);
+						return colorScale(count);
+					} else {
+						return "#666";
+					}
+				});
+
+				svg.select(".value-box").remove();
+			});
+		});
+	};
+
+	useEffect(() => {
+		if (status === "success" && data !== undefined) {
+			printD3(data);
+		}
+	}, [status, data]);
+	if (status === "loading") {
+		return <Loading />;
+	}
+	return <div ref={chart}></div>;
 };
 
 export default KoreaMap;
--- strayanimal-app/strayanimal/src/components/ProtectAnimalList.js
+++ strayanimal-app/strayanimal/src/components/ProtectAnimalList.js
@@ -1,74 +1,149 @@
 import AnimalCard from "./AnimalCard";
-import Loading from './Loading';
+import Loading from "./Loading";
 import { useQuery } from "@tanstack/react-query";
 import "../css/Overview.css";
 import "../css/AnimalCard.css";
 import { useEffect, useState } from "react";
-import { fetchPostData , getTotalDoc } from "../loader/postDataLoader";
+import { fetchPostData, getTotalDoc } from "../loader/postDataLoader";
 
 function ProtectAnimalList() {
-    const [page, setPage] = useState(0)
-    const [kindType, setKindType] = useState("개");
-    const [lastSnap, setLastSnap] = useState(undefined);
-    const [lookLast, setLookLast] = useState(true);
-    const [endPage, setEndPage] = useState(-1);
-    const {
-        isLoading,
-        data,
-        isFetching,
-    } = useQuery({
-        queryKey: ['strayanimal', kindType, page],
-        queryFn: () => fetchPostData(page, kindType, lastSnap, lookLast),
-        keepPreviousData : true
-    })
-    
-    useEffect(() => {
-        window.scrollTo(0, 0)
-    }, [kindType, page])
+	const [page, setPage] = useState(0);
+	const [kindType, setKindType] = useState("개");
+	const [lastSnap, setLastSnap] = useState(undefined);
+	const [lookLast, setLookLast] = useState(true);
+	const [endPage, setEndPage] = useState(-1);
+	const { isLoading, data, isFetching } = useQuery({
+		queryKey: ["strayanimal", kindType, page],
+		queryFn: () => fetchPostData(page, kindType, lastSnap, lookLast),
+		keepPreviousData: true,
+	});
 
-    useEffect(() => {
-        setPage(0);
-        getTotalDoc().then((e) => setEndPage(~~(e/10)));
-    }, [kindType])
+	useEffect(() => {
+		window.scrollTo(0, 0);
+	}, [kindType, page]);
 
-    return (
-        <div className="protect-animal-list-wrap vertical align-start" style={{width:"100%", maxWidth:"1240px", padding:"20px"}}>
-            <div className="highlight-title" style={{transform: "translateY(0)", padding:"0 20px"}}>유기동물 공고 목록</div>
-            <div className="number-highlight-line" style={{margin: "20px 0"}}></div>
-            
-            <div className="protect-animal-list">
-                <div className="horizontal align-start" style={{width:"100%"}}>
-                    <button className="type-select-button" onClick={()=>setKindType("개")} style={{backgroundColor: kindType==="개"? "#FF5F15": "#4E4E4E"}}>개</button>
-                    <button className="type-select-button" onClick={()=>setKindType("고양이")} style={{backgroundColor: kindType==="고양이"? "#FF5F15": "#4E4E4E"}}>고양이</button>
-                </div>
-                {
-                    isLoading || isFetching? <div style={{height:"400px", width:"100%"}}><Loading/></div>:
-                    data.pages.map((elem) => (
-                        <AnimalCard key={elem.desertionNo} src={elem.popfile} age={elem.age} color={elem.colorCd}
-                        careNm={elem.careNm+" ( "+elem.careAddr+" )"} 
-                        kind={elem.kindSpcs}
-                        sexCd={elem.sexCd} status={elem.specialMark} weight={elem.weight}
-                        />
-                    ))
-                    
-                }
-            </div>
-            
-            <div className="horizontal align-center justify-center-wrap" style={{width:"100%", margin:"0 0 60px 0"}}>
-                {page===0? null: <button className="page-button type-select-button"
-                    onClick={() => {setPage(0);}}>처음</button>}
-                {page===0? null: <button className="page-button type-select-button"
-                    onClick={() => {setPage(page-1); setLookLast(false); setLastSnap(data.firstId)}}>
-                    이전
-                </button>}
-                <div className="page-button type-select-button">{page+1}</div>
-                {page===endPage? null: <button className="page-button type-select-button"
-                    onClick={() => {setPage(page+1); setLookLast(true); setLastSnap(data.lastId)}}>다음</button>}
-                {page===endPage? null: <button className="page-button type-select-button"
-                    onClick={() => {setPage(endPage); setLookLast(true); setLastSnap(-1)}}>끝</button>}
-            </div>
-        </div>
-    )
+	useEffect(() => {
+		setPage(0);
+		getTotalDoc().then((e) => setEndPage(~~(e / 10)));
+	}, [kindType]);
+
+	return (
+		<div
+			className="protect-animal-list-wrap vertical align-start"
+			style={{ width: "100%", maxWidth: "1240px", padding: "20px" }}
+		>
+			<div
+				className="highlight-title"
+				style={{ transform: "translateY(0)", padding: "0 20px" }}
+			>
+				유기동물 공고 목록
+			</div>
+			<div
+				className="number-highlight-line"
+				style={{ margin: "20px 0" }}
+			></div>
+
+			<div className="protect-animal-list">
+				<div
+					className="horizontal align-start"
+					style={{ width: "100%" }}
+				>
+					<button
+						className="type-select-button"
+						onClick={() => setKindType("개")}
+						style={{
+							backgroundColor:
+								kindType === "개" ? "#FF5F15" : "#4E4E4E",
+						}}
+					>
+						개
+					</button>
+					<button
+						className="type-select-button"
+						onClick={() => setKindType("고양이")}
+						style={{
+							backgroundColor:
+								kindType === "고양이" ? "#FF5F15" : "#4E4E4E",
+						}}
+					>
+						고양이
+					</button>
+				</div>
+				{isLoading || isFetching ? (
+					<div style={{ height: "400px", width: "100%" }}>
+						<Loading />
+					</div>
+				) : (
+					data.pages.map((elem) => (
+						<AnimalCard
+							key={elem.desertionNo}
+							src={elem.popfile}
+							age={elem.age}
+							color={elem.colorCd}
+							careNm={elem.careNm + " ( " + elem.careAddr + " )"}
+							kind={elem.kindSpcs}
+							sexCd={elem.sexCd}
+							status={elem.specialMark}
+							weight={elem.weight}
+						/>
+					))
+				)}
+			</div>
+
+			<div
+				className="horizontal align-center justify-center-wrap"
+				style={{ width: "100%", margin: "0 0 60px 0" }}
+			>
+				{page === 0 ? null : (
+					<button
+						className="page-button type-select-button"
+						onClick={() => {
+							setPage(0);
+						}}
+					>
+						처음
+					</button>
+				)}
+				{page === 0 ? null : (
+					<button
+						className="page-button type-select-button"
+						onClick={() => {
+							setPage(page - 1);
+							setLookLast(false);
+							setLastSnap(data.firstId);
+						}}
+					>
+						이전
+					</button>
+				)}
+				<div className="page-button type-select-button">{page + 1}</div>
+				{page === endPage ? null : (
+					<button
+						className="page-button type-select-button"
+						onClick={() => {
+							setPage(page + 1);
+							setLookLast(true);
+							setLastSnap(data.lastId);
+						}}
+					>
+						다음
+					</button>
+				)}
+				{page === endPage ? null : (
+					<button
+						className="page-button type-select-button"
+						onClick={() => {
+							setPage(endPage);
+							setLookLast(true);
+							setLastSnap(-1);
+						}}
+					>
+						끝
+					</button>
+				)}
+			</div>
+		</div>
+	);
 }
 
 export default ProtectAnimalList;
