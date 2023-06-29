e75c60a3cf16691f8e20c34671fe93f1f3cc8a84
Dahye K <kangdiamond@gmail.com>
2023-06-30 01:21:19 +0900

    jest config add

--- strayanimal-app/strayanimal/src/App.test.js
+++ strayanimal-app/strayanimal/src/App.test.js
@@ -1,8 +1,21 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react';
+import { renderHook } from "@testing-library/react";
+import { getTotalDoc } from 'src/loader/postDataLoader';
+import  { QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+const queryClient = new QueryClient();
+const wrapper = ({ children }) => (
+  <QueryClientProvider client={queryClient}>
+    {children}
+  </QueryClientProvider>
+);
+
+const { result, waitFor } = renderHook(() => getTotalDoc(), { wrapper });
+
+await waitFor(() => {
+  return result.current.isSuccess;
 });
+
+expect(result.current.data).toGreaterThan(100);
