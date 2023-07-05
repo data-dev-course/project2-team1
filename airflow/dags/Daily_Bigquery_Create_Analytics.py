943462cb563fea0834eaa4850a92ced6a86e2325
TJ Park <hahxowns@naver.com>
2023-07-05 17:29:59 +0900

    Update Daily_Bigquery_Create_Analytics.py

--- airflow/dags/Daily_Bigquery_Create_Analytics.py
+++ airflow/dags/Daily_Bigquery_Create_Analytics.py
@@ -3,9 +3,7 @@ from datetime import datetime
 from airflow import DAG
 from airflow.exceptions import AirflowException
 from airflow.operators.trigger_dagrun import TriggerDagRunOperator
-from airflow.providers.google.cloud.operators.bigquery import (
-    BigQueryExecuteQueryOperator,
-)
+from airflow.providers.google.cloud.operators.bigquery import BigQueryExecuteQueryOperator
 
 from plugins import slack
 
